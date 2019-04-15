67c51b5a7dcb137b21cd02d4881c02dc353bc582
khwarizmi <kawahara6514@gmail.com>
2019-04-16 00:41:12 +0900

    add git config

--- setup.sh
+++ setup.sh
@@ -5,3 +5,8 @@ cp ./configs/tmux.conf ~/.tmux.conf
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp ./configs/vimrc ~/.vimrc
 vim +PluginInstall +qall
+
+#git config
+git config --global user.name "Kawaharasouta"
+git config --global user.email "kawahara6514@gmail.com"
+git config --global core.editor 'vim -c "set fenc=utf-8"'
