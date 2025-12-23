0a8c062380b8bbc440bd71f66228d5e6551234cc
Kawaharasouta <kawahara6514@gmail.com>
2025-12-23 09:39:21 +0900

    feat: Add i3 & i3blocks configs

--- scripts/misc/xdg_config.sh
+++ scripts/misc/xdg_config.sh
@@ -6,6 +6,11 @@ source ~/git/dotfiles/configs/bash/bash_envs
 mkdir -p ${XDG_CONFIG_HOME}
 
 
+##### i3 configs #####
+ln -sf ${CONFIGS_PATH}/xdg_config/i3 ${XDG_CONFIG_HOME}/i3
+##### i3blocks configs #####
+ln -sf ${CONFIGS_PATH}/xdg_config/i3blocks ${XDG_CONFIG_HOME}/i3blocks
+
 ##### sway configs #####
 ln -sf ${CONFIGS_PATH}/xdg_config/sway ${XDG_CONFIG_HOME}/sway
 
